b07328acfd90e26784bac79f3540bba91ecb56a3
Andrea <andre.mora89@gmail.com>
2023-05-26 20:10:29 +0200

    2,3 iteration

--- src/App.js
+++ src/App.js
@@ -37,7 +37,17 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet) => {
+        return (
+          <Tweet
+            name={tweet.user.name}
+            image={tweet.user.image}
+            handle={tweet.user.handle}
+            timestamp={tweet.timestamp}
+            message={tweet.message}
+          />
+        );
+      })}
     </div>
   );
 }
